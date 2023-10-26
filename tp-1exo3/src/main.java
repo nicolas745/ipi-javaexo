1a51b93d2967d841a15a6af177c3c0de9f455347
nicolas blanc <47307856+nicolas745@users.noreply.github.com>
2023-10-26 11:53:09 +0200

    tp1-exo3 end

--- tp-1exo3/src/main.java
+++ tp-1exo3/src/main.java
@@ -4,17 +4,49 @@ import java.util.List;
 public class main {
 
 	public static void main(String[] args) {
-		char[] chars= "1b".toCharArray();
+		char[] chars= "erd".toCharArray();
 		List<Integer> nbs = new ArrayList<Integer>();
 		nbs.add(23);
 		nbs.add(132);
 		nbs.add(42);
-		nbs.add(5);
+		nbs.add(-5);
 		nbs.add(19);
-		System.out.println(get_alpha_H(chars));;
+		System.out.println("léttre la plus haute : "+get_alpha_high(chars));
+		System.out.println("le nombre max : "+get_nb_max(nbs));
+		System.out.println("le nombre min : "+get_nb_min(nbs));
+		System.out.println("le lettre existe : "+is_chars(chars,'a'));
 	}
 
-	private static char get_alpha_H(char[] chars) {
+	private static boolean is_chars(char[] chars, char c) {
+		for(char ch : chars) {
+			if(Character.toUpperCase(ch) == Character.toUpperCase(c)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static int get_nb_min(List<Integer> nbs) {
+		int res = nbs.get(0);
+		for( int nb : nbs) {
+			if(res>nb) {
+				res = nb;
+			}
+		}
+		return res;
+	}
+
+	private static int get_nb_max(List<Integer> nbs) {
+		int res = nbs.get(0);
+		for( int nb : nbs) {
+			if(res<nb) {
+				res = nb;
+			}
+		}
+		return res;
+	}
+
+	private static char get_alpha_high(char[] chars) {
 		char res = Character.toUpperCase(chars[0]);
 		for(int i=0;i<chars.length;i++) {
 			char lettre = Character.toUpperCase(chars[i]);
@@ -24,6 +56,11 @@ public class main {
 				}
 			}
 		}
-		return res;
+		if(Character.isAlphabetic(res)) {
+			return res;
+		}else {
+			System.out.print("error il y a pas de ");
+			return 0;
+		}
 	}
 }
